62eb0e605ab541d9032d97367a929f53e7396efc
Jesus Marron <jesusmarron7@gmail.com>
2023-06-06 10:03:07 -0500

    initial commit

---
+++ src/main/java/com/jesusmarron/Main.java
@@ -0,0 +1,27 @@
+package com.jesusmarron;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int x = -5;
+
+        x += 2; // -3
+        x -= 2; // -5
+        x /= 2; // -2
+        x *= 2; // -4
+        // x %= 2; // 0
+
+        String hello = "Hello World";
+
+        System.out.println(x);
+        System.out.println(hello);
+
+        int totalSugarInGlass = 0;
+        int sugar = 5;
+        int maxSugar = 10;
+
+        if (sugar <= maxSugar) {
+            totalSugarInGlass += sugar;
+        }
+    }
+}
