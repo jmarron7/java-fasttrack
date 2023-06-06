fed20c33b8c2969d0ad5d38beb00779f78c425d9
Jesus Marron <jesusmarron7@gmail.com>
2023-06-06 11:00:33 -0500

    arrays

--- src/main/java/com/jesusmarron/Main.java
+++ src/main/java/com/jesusmarron/Main.java
@@ -24,11 +24,19 @@ public class Main {
 //            totalSugarInGlass += sugar;
 //        }
 
-        int[] numbers = new int[5];
+        int[] numbers = new int[] {1, 2, 3, 4, 5};
 
-        int count = 1;
-        for (int i = 0; i < numbers.length; i++, count++) {
-            numbers[i] = count;
+        int max = (int) Math.random() * 10;
+
+        int currentNumber = 6;
+
+        for (int i = 0; i < max; i++, currentNumber++) {
+            int temp[] = new int[numbers.length + 1];
+            for (int j = 0; j < numbers.length; j++) {
+                temp[j] = numbers[j];
+            }
+            temp[temp.length - 1] = currentNumber;
+            numbers = temp;
         }
 
         for (int i = 0; i < numbers.length; i++) {
