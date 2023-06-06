526e0a01d8d4f7c9379a1d8dd8884d206e71134e
Jesus Marron <jesusmarron7@gmail.com>
2023-06-06 10:24:44 -0500

    arrays

--- src/main/java/com/jesusmarron/Main.java
+++ src/main/java/com/jesusmarron/Main.java
@@ -3,25 +3,36 @@ package com.jesusmarron;
 public class Main {
     public static void main(String[] args) {
 
-        int x = -5;
+//        int x = -5;
+//
+//        x += 2; // -3
+//        x -= 2; // -5
+//        x /= 2; // -2
+//        x *= 2; // -4
+//        // x %= 2; // 0
+//
+//        String hello = "Hello World";
+//
+//        System.out.println(x);
+//        System.out.println(hello);
+//
+//        int totalSugarInGlass = 0;
+//        int sugar = 5;
+//        int maxSugar = 10;
+//
+//        if (sugar <= maxSugar) {
+//            totalSugarInGlass += sugar;
+//        }
 
-        x += 2; // -3
-        x -= 2; // -5
-        x /= 2; // -2
-        x *= 2; // -4
-        // x %= 2; // 0
+        int[] numbers = new int[5];
 
-        String hello = "Hello World";
+        int count = 1;
+        for (int i = 0; i < numbers.length; i++, count++) {
+            numbers[i] = count;
+        }
 
-        System.out.println(x);
-        System.out.println(hello);
-
-        int totalSugarInGlass = 0;
-        int sugar = 5;
-        int maxSugar = 10;
-
-        if (sugar <= maxSugar) {
-            totalSugarInGlass += sugar;
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
         }
     }
 }
